100040dabfe4563615b87701eb94aea6ebf2221e
Sagarika Jandhyala <srivisaab@gmail.com>
2026-01-18 19:43:09 +0530

    commit

--- embedding/embed_pee.py
+++ embedding/embed_pee.py
@@ -6,14 +6,27 @@ def embed_payload(image, bits):
     idx = 0
     h, w = image.shape
 
-    for i in range(8, h-8):
-        for j in range(8, w-8):
+    for i in range(8, h - 8):
+        for j in range(8, w - 8):
             if idx >= len(bits):
                 return stego, idx
 
             pred = predict_pixel(image, i, j)
             err = image[i, j] - pred
-            stego[i, j] = pred + (2 * err + bits[idx])
-            idx += 1
+
+            # PEE embedding
+            if err == 0:
+                stego[i, j] = pred + bits[idx]
+                idx += 1
+            elif err == -1:
+                stego[i, j] = pred - 1 - bits[idx]
+                idx += 1
+            else:
+                # histogram shifting
+                if err > 0:
+                    stego[i, j] = image[i, j] + 1
+                else:
+                    stego[i, j] = image[i, j] - 1
 
     return stego, idx
+
--- extraction/extract_pee.py
+++ extraction/extract_pee.py
@@ -1,3 +1,4 @@
+# extract_pee.py
 from models.predictor import predict_pixel
 
 def extract_payload(stego, num_bits):
@@ -6,13 +7,29 @@ def extract_payload(stego, num_bits):
     idx = 0
     h, w = stego.shape
 
-    for i in range(8, h-8):
-        for j in range(8, w-8):
+    for i in range(8, h - 8):
+        for j in range(8, w - 8):
             if idx >= num_bits:
                 return recovered, bits
+
             pred = predict_pixel(recovered, i, j)
-            new_err = stego[i, j] - pred
-            bits.append(new_err % 2)
-            recovered[i, j] = pred + (new_err // 2)
-            idx += 1
+            err = stego[i, j] - pred
+
+            # Decode
+            if err == 0 or err == 1:
+                bits.append(err)
+                recovered[i, j] = pred
+                idx += 1
+            elif err == -1 or err == -2:
+                bits.append(-err - 1)
+                recovered[i, j] = pred - 1
+                idx += 1
+            else:
+                # reverse shifting
+                if err > 1:
+                    recovered[i, j] = stego[i, j] - 1
+                elif err < -2:
+                    recovered[i, j] = stego[i, j] + 1
+
     return recovered, bits
+
